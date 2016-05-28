7abe8343b4da896c75d6d653a8c9bffa80660e66
dataHub.server <zhaozhen@yjysh.com>
2016-05-28 15:01:04 +0800

    add install software shell

---
+++ install_centos_software.sh
@@ -0,0 +1,10 @@
+# base software
+yum install -y vim epel-release gcc mlocate htop rsync bash-completion yum-fastestmirror lrzsz vim mlocate iproute iftop autojump bind-utils lm_sensors 
+
+# if nead kvm run this command:
+yum install -y libcanberra-gtk2 qemu-kvm.x86_64 qemu-kvm-tools.x86_64 libvirt.x86_64 libvirt-cim.x86_64 libvirt-client.x86_64 libvirt-java.noarch libvirt-python.x86_64 libiscsi-1.7.0-5.el6.x86_64 dbus-devel virt-clone tunctl virt-manager libvirt libvirt-python python-virtinst
+
+# if nead x windows run this command:
+yum groupinstall -y "Desktop" "X Window System" "Chinese Support" "GNOME Desktop"
+
+
